9802e89ddb793cc72c4c800c5cd44ccac6fbe3d5
Freedom Scheyd <joefreedomsch@gmail.com>
2021-04-20 22:32:03 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,5 +12,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 0) do
+   
+    create_table "students", force: :cascade do |t|
+        t.string "first_name"
+        t.string "last_name"
+        t.integer "created_at", null: false
+        t.integer "updated_at", null: false
+    end
 
 end
